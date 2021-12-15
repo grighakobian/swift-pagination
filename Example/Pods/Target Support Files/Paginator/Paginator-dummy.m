953df2a26871089635a5f48ed33dd38b424e6d86
Grigor Hakobyan <grighakobian@gmail.com>
2021-12-15 17:43:43 +0400

    feat: Add example project

---
+++ Example/Pods/Target Support Files/Paginator/Paginator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Paginator : NSObject
+@end
+@implementation PodsDummy_Paginator
+@end
---
+++ Example/Pods/Target Support Files/Pods-Paginator_Tests/Pods-Paginator_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Paginator_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Paginator_Tests
+@end
