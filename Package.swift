1c4d72d072366b383afbd2768c1c030f7a6ea953
Grigor Hakobyan <grighakobian@gmail.com>
2022-08-09 21:44:50 +0400

    fix: target source path

--- Package.swift
+++ Package.swift
@@ -9,6 +9,6 @@ let package = Package(
         .library(name: "Paginator", targets: ["Paginator"]),
     ],
     targets: [
-        .target(name: "Paginator", dependencies: [])
+        .target(name: "Paginator", dependencies: [], path: "Sources")
     ]
 )
