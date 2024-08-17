6a124fbe2e2e2634a73d5c6d81babf7d06958cee
Grigor Hakobyan <grighakobian@gmail.com>
2024-08-17 20:39:33 +0400

    feat: created new swift example project

---
+++ Example/Swift/Examples/Examples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Examples
+//
+//  Created by Grigor Hakobyan on 17.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
