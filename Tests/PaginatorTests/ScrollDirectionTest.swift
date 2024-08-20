a92e46c588335c394e2637e2dde9fa7ada47c08b
Grigor Hakobyan <grighakobian@gmail.com>
2024-08-20 19:19:50 +0400

    tests: add unit tests for scroll direction class

---
+++ Tests/PaginatorTests/ScrollDirectionTest.swift
@@ -0,0 +1,41 @@
+import XCTest
+@testable import Paginator
+
+final class ScrollDirectionTest: XCTestCase {
+
+    func testInsertDirectionMethod() {
+        let scrollDirection: ScrollDirection = .left
+        scrollDirection.insert(.right)
+        XCTAssertTrue(scrollDirection.contains(.left))
+        XCTAssertTrue(scrollDirection.contains(.right))
+        XCTAssertTrue(scrollDirection.contains(.horizontal))
+        XCTAssertFalse(scrollDirection.contains(.vertical))
+    }
+
+    func testContainsDirectionMethod() {
+        let scrollDirection: ScrollDirection = .vertical
+        XCTAssertFalse(scrollDirection.contains(.left))
+        XCTAssertFalse(scrollDirection.contains(.right))
+        XCTAssertTrue(scrollDirection.contains(.up))
+        XCTAssertTrue(scrollDirection.contains(.down))
+    }
+
+    func testEquatableConformance() {
+        let scrollDirection: ScrollDirection = .left
+        XCTAssertTrue(scrollDirection == .left)
+        XCTAssertFalse(scrollDirection == .right)
+        scrollDirection.insert(.right)
+        XCTAssertTrue(scrollDirection == .horizontal)
+        XCTAssertFalse(scrollDirection == .vertical)
+        XCTAssertFalse(scrollDirection.isEqual(0))
+    }
+
+    func testExpressibleByArrayLiteralComformance() {
+        let scrollDirection: ScrollDirection = [.up, .down]
+        XCTAssertTrue(scrollDirection.contains(.up))
+        XCTAssertTrue(scrollDirection.contains(.down))
+        XCTAssertTrue(scrollDirection.contains(.vertical))
+        XCTAssertFalse(scrollDirection.contains(.left))
+        XCTAssertFalse(scrollDirection.contains(.right))
+    }
+}
