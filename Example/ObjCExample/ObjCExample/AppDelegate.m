4cce6db94f629c980917497ca216dbbd24693973
Grigor Hakobyan <grighakobian@gmail.com>
2026-04-04 01:29:10 +0400

    feat: add objc-example app

---
+++ Example/ObjCExample/ObjCExample/AppDelegate.m
@@ -0,0 +1,22 @@
+#import "AppDelegate.h"
+#import "ColorsViewController.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
+
+    ColorsViewController *colorsVC = [[ColorsViewController alloc] init];
+    colorsVC.title = @"Colors (Obj-C)";
+
+    UINavigationController *nav =
+        [[UINavigationController alloc] initWithRootViewController:colorsVC];
+    nav.navigationBar.prefersLargeTitles = YES;
+
+    self.window.rootViewController = nav;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
---
+++ Example/ObjCExample/ObjCExample/ColorsViewController.h
@@ -0,0 +1,5 @@
+#import <UIKit/UIKit.h>
+
+@interface ColorsViewController : UICollectionViewController
+
+@end
